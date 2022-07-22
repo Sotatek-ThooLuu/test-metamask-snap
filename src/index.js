18cf520329bd34382fe90d253c8520e97763e474
Sotatek-ThooLuu <thooluu@sotatek.com>
2022-07-22 17:11:59 +0700

    patch packages

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
+const { getCaverFunction } = require('./caverHelper');
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
-    case 'hello':
+    case 'hello': {
+      const fees = JSON.parse(await getFees());
+      const baseFee = parseFloat(fees.currentBaseFee);
+      const safeLow = Math.ceil(baseFee + parseFloat(fees.safeLow));
+      const standard = Math.ceil(baseFee + parseFloat(fees.standard));
+      const fastest = Math.ceil(baseFee + parseFloat(fees.fastest));
+      const caverVersion = await getCaverFunction();
+
       return wallet.request({
         method: 'snap_confirm',
         params: [
           {
-            prompt: `Hello, ${originString}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+            prompt: `Gas Fees`,
+            description: 'Current Gas Fees from etherchain.org:',
+            textAreaContent: `Low: ${safeLow}\n
+              Average: ${standard}\n
+              High: ${fastest}\n
+              Caver version: ${caverVersion}
+              `,
           },
         ],
       });
+    }
     default:
       throw new Error('Method not found.');
   }
 });
+
+async function getFees() {
+  const response = await fetch('https://www.etherchain.org/api/gasPriceOracle');
+  return response.text();
+}
